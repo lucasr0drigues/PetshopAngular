8a53b65d3b0ddf30d5dd4589d89ae60661b979e2
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-08 14:39:35 -0300

    delete blank css and spec files

--- src/app/pages/account/pets-page/pets-page.component.ts
+++ src/app/pages/account/pets-page/pets-page.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pets-page',
   templateUrl: './pets-page.component.html',
-  styleUrls: ['./pets-page.component.css'],
 })
 export class PetsPageComponent implements OnInit {
   constructor() {}
