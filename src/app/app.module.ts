1f2d95d65fe7d819d36fc1dc527b6da680c42aba
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-09 18:27:46 -0300

    create product card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
@@ -11,6 +11,7 @@ import { PetsPageComponent } from './pages/account/pets-page/pets-page.component
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { FramePageComponent } from './pages/master/frame.page';
+import { ProductCardComponentComponent } from './components/store/product-card-component/product-card-component.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { FramePageComponent } from './pages/master/frame.page';
     ProductsPageComponent,
     CartPageComponent,
     FramePageComponent,
+    ProductCardComponentComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
---
+++ src/app/components/store/product-card-component/product-card-component.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-component',
+  templateUrl: './product-card-component.component.html',
+})
+export class ProductCardComponentComponent implements OnInit {
+  @Input() product: any;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
