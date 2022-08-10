f907dfeeda821d121704588073cceac6b0efe51f
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-10 13:04:29 -0300

    create and use product type

--- src/app/components/store/product-card-component/product-card-component.component.ts
+++ src/app/components/store/product-card-component/product-card-component.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-card-component',
   templateUrl: './product-card-component.component.html',
 })
 export class ProductCardComponentComponent implements OnInit {
-  @Input() product: any;
+  @Input() product!: Product;
   constructor() {}
 
   ngOnInit(): void {}
