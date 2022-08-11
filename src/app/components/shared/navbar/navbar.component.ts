619a1024df3a93df77af0c26ca6f81328636ad55
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-11 20:06:26 -0300

    add username to navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { Security } from 'src/app/utils/security.util';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
+  public user!: User;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user = Security.getUser();
+  }
 }
