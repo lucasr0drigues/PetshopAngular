bb7b7e8480cc0267abeaa3c7ac0a003817fbc5a3
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-11 15:39:36 -0300

    add loading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { FramePageComponent } from './pages/master/frame.page';
 import { ProductCardComponentComponent } from './components/store/product-card-component/product-card-component.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CartPageComponent,
     FramePageComponent,
     ProductCardComponentComponent,
+    LoadingComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/account/login-page/login-page.component.ts
+++ src/app/pages/account/login-page/login-page.component.ts
@@ -9,6 +9,7 @@ import { DataService } from 'src/app/services/data.services';
 })
 export class LoginPageComponent implements OnInit {
   public form!: FormGroup;
+  public busy = false;
 
   constructor(private service: DataService, private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -31,5 +32,36 @@ export class LoginPageComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const token = localStorage.getItem('petshop.token');
+    if (token) {
+      this.busy = true;
+      this.service.refreshToken().subscribe(
+        (data: any) => {
+          console.log(data);
+          localStorage.setItem('petshop.token', data.token);
+          this.busy = false;
+        },
+        (err) => {
+          localStorage.clear();
+          this.busy = false;
+        }
+      );
+    }
+  }
+
+  submit() {
+    this.busy = true;
+    this.service.authenticate(this.form.value).subscribe(
+      (data: any) => {
+        console.log(data);
+        localStorage.setItem('petshop.token', data.token);
+        this.busy = false;
+      },
+      (err) => {
+        console.log(err);
+        this.busy = false;
+      }
+    );
+  }
 }
