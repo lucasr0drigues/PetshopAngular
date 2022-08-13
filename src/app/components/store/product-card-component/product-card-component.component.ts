38431d00d57d15e0df2682eedbc2a13496135d09
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-13 04:45:09 -0300

    finishing product cart

--- src/app/components/store/product-card-component/product-card-component.component.ts
+++ src/app/components/store/product-card-component/product-card-component.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product.model';
+import { CartUtil } from 'src/app/utils/cart.util';
 
 @Component({
   selector: 'app-product-card-component',
@@ -7,7 +9,22 @@ import { Product } from 'src/app/models/product.model';
 })
 export class ProductCardComponentComponent implements OnInit {
   @Input() product!: Product;
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor(private toastr: ToastrService) {}
+
+  ngOnInit() {}
+
+  addToCart() {
+    CartUtil.add(
+      this.product._id,
+      this.product.title,
+      1,
+      this.product.price,
+      this.product.images[0]
+    );
+    this.toastr.success(
+      `${this.product.title} adicionado ao carrinho`,
+      'Produto Adicionado'
+    );
+  }
 }
